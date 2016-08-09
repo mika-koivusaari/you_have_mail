a2ce124f7fed1e6117f6659db42c849379d996af
Mika Koivusaari <mika.koivusaari@gmail.com>
2016-08-09 07:27:52 +0300

    After sleep, don't start modem. Start modem only if state changed and then send message.

--- check_mail.lua
+++ check_mail.lua
@@ -1,9 +1,24 @@
+--needed modules:
+--mqtt, gpio, wifi, node, rtcmem
+
 --define pins
 local ldr_pin=1 --GPIO5
 local led_pin=6 --GPIO12
 local stop_pin=2 --GPIO4
 local tsl257_power_pin=5 --GPIO14
-local sleep_seconds=60
+local sleep_seconds=10
+
+local TOO_MUCH_LIGHT=1
+local MAIL=2
+local NO_MAIL=3
+local ERROR=4 --general unkown error
+
+local MODEM_AFTER_SLEEP=2
+--1, RF_CAL after deep-sleep wake up, there will belarge current
+--2, no RF_CAL after deep-sleep wake up, there will only be small current
+--4, disable RF after deep-sleep wake up, just like modem sleep, there will be the smallest current
+--NOTE https://github.com/nodemcu/nodemcu-firmware/issues/1225
+local status=0
 
 --set pin modes
 gpio.mode(ldr_pin, gpio.INPUT)
@@ -13,38 +28,56 @@ gpio.write(led_pin, gpio.LOW) --led off
 --Moving tsl257 power from 3.3v rail to gpio prevents
 --sensors from draining battery when not needed.
 gpio.mode(tsl257_power_pin, gpio.OUTPUT)
-gpio.write(tsl257_power_pin, gpio.HIGH) --TSL257 on
 
---mqtt connection is ready, check for mail
-function mqtt_connect(client)
-  print("Connected to MQTT broker")
-  --Check for ldr, if there is light without the led being on
-  --then mailbox lid is open and we don't know if there is mail.
+function check_mail()
+  gpio.write(tsl257_power_pin, gpio.HIGH) --TSL257 on
+
   state=gpio.read(ldr_pin)
   if state==1 then
     --there is too much light to know if led is on
     print("Too much light")
-    m:publish("/house/mail","too much light",0,0,mqtt_puback)
+    return TOO_MUCH_LIGHT
   else
     --dark, next we will check if we see led
     print("It's dark, lets see if we have mail.")
     gpio.write(led_pin, gpio.HIGH) --led on
     state=gpio.read(ldr_pin)  --read ldr
+    gpio.write(led_pin, gpio.LOW) --led off
     if state==1 then  --we see led, no mail
       print("No mail")
-      m:publish("/house/mail","no mail:(",0,0,mqtt_puback)
+      return NO_MAIL
     else  --we don't see led, there is something between led and ldr -> mail
       print("Mail")
-      m:publish("/house/mail","you have mail",0,0,mqtt_puback)
+      return MAIL
     end
   end
-  gpio.write(led_pin, gpio.LOW) --led off
+  return ERROR --we should newer get to here
+end
+
+--mqtt connection is ready, check for mail
+function mqtt_connect(client)
+  print("Connected to MQTT broker")
+  if status==TOO_MUCH_LIGHT then
+    --there is too much light to know if led is on
+    print("Too much light")
+    m:publish("/house/mail","too much light",0,0,mqtt_puback)
+  elseif status==NO_MAIL then
+    print("No mail")
+    m:publish("/house/mail","no mail:(",0,0,mqtt_puback)
+  elseif status==MAIL then
+    print("Mail")
+    m:publish("/house/mail","you have mail",0,0,mqtt_puback)
+  else
+    print("Error")
+    m:publish("/house/mail","error",0,0,mqtt_puback)
+  end
 end
 
 --callback when we get mqtt close
 function mqtt_close (client)
   print("Connection to MQTT broker closed. Sleeping.")
-  node.dsleep(sleep_seconds*1000000) --sleep for x seconds
+  wifi.sta.disconnect()
+  node.dsleep(sleep_seconds*1000000,MODEM_AFTER_SLEEP) --sleep for x seconds
 end
 --callback when we get mqtt puback
 function mqtt_puback(client)
@@ -56,7 +89,8 @@ end
 --callback if mqtt connection fails
 function mqtt_fail(client, reason)
   print("MQTT broker connection failed reason: "..reason)
-  node.dsleep(sleep_seconds*1000000) --sleep for x seconds
+  wifi.sta.disconnect()
+  node.dsleep(sleep_seconds*1000000,MODEM_AFTER_SLEEP) --sleep for x seconds
 end
 
 --wifi status callback
@@ -69,7 +103,8 @@ function wifi_status(previous_state)
                                         mqtt_fail)
   else --else we have an error -> sleep and try again
     print("Wifi connection error, status "..wifi.sta.status().." previous "..previous_state)
-    node.dsleep(sleep_seconds*1000000) --sleep for x seconds
+    wifi.sta.disconnect()
+    node.dsleep(sleep_seconds*1000000,MODEM_AFTER_SLEEP) --sleep for x seconds
   end
 end
 
@@ -80,26 +115,31 @@ if gpio.read(stop_pin)==1 then
   if not tmr.alarm(0, 10000, tmr.ALARM_SINGLE,
                    function()
                      print("Error, forced sleep")
-                     node.dsleep(sleep_seconds*1000000) --sleep for x seconds
+                     wifi.sta.disconnect()
+                     node.dsleep(sleep_seconds*1000000,MODEM_AFTER_SLEEP) --sleep for x seconds
                    end) then
     print("Could not start timer!")
   end
-  -- init mqtt client with keepalive timer 120sec
-  print("Create MQTT client.")
-  m = mqtt.Client("mailESP", 120, nil, nil)
 
-  --check if we have and ip, if we have just started then probably not
-  if wifi.sta.getip() ~= nil then
-    print("Connect to MQTT server.")
-    m:connect("192.168.0.106", 1883, 0, mqtt_connect, 
-                                        mqtt_fail)
-  else --register a listener for wifi events
-    print("Not connected to wifi, connect.")
+  status=check_mail()
+  last_status=rtcmem.read32(127)
+  if status~=last_status then
+    rtcmem.write32(127,status)
+    print("Status changed, connect to wifi.")
     wifi.sta.eventMonReg(wifi.STA_GOTIP,wifi_status)
     wifi.sta.eventMonReg(wifi.STA_WRONGPWD,wifi_status)
     wifi.sta.eventMonReg(wifi.STA_APNOTFOUND,wifi_status)
     wifi.sta.eventMonReg(wifi.STA_FAIL,wifi_status)
     wifi.sta.eventMonStart()
+    wifi.setmode(wifi.STATION)
+    print(wifi.sta.getip())
+
+    wifi.sta.connect()
+    m=mqtt.Client("MailESP", 120)
+  else
+    print("No change, sleep")
+    wifi.sta.disconnect()
+    node.dsleep(sleep_seconds*1000000,MODEM_AFTER_SLEEP) --sleep for x seconds
   end
 else
   print("Stop pin pulled to ground.")
